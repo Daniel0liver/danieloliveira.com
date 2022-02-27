fd91c67972dfc00d2e8c56dc936c1a1dd0ab0538
Daniel Oliveira <daniel.noliveira@mercadolivre.com>
2022-02-27 09:29:30 -0300

    feat(projects): add my projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,21 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Dev Finder',
+    description: `Find developers by github username and see their top projects.`,
+    imgSrc: '/static/images/search_devs.png',
+    href: 'https://search-devs-challenge.vercel.app/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'A complete boilerplate for NextJS + Typescript',
+    description: `Start your NextJS + Typescript projects with a complete boilerplate without having to configure the entire project from scratch.`,
+    imgSrc: '/static/images/boilerplate.png',
+    href: 'https://github.com/Daniel0liver/boilerplate-nextjs#getting-started',
+  },
+  {
+    title: 'Surf climate RestAPI',
+    description: `Api provides weather data for all beaches in the world, you can know in real time if this beach is good for surfing. Feel free to integrate with your app, remembering that it has a rate limit of 50 requests per day.`,
+    imgSrc: '/static/images/surf_climate.png',
+    href: 'https://github.com/Daniel0liver/surf-climate-api#surf-climate-api',
   },
 ]
 
